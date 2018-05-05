dc2be7e762eb846b55e21c0d4ce6de840a5ccb69
aconfee <adamestela@gmail.com>
2018-05-04 19:44:05 -0500

    saving progress. static page development

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { API_ROOT } from './appsettings.js';
 import './enzymeSetup.js';
 
-import DummyQuery from './components/Dummy/dummy.query.js';
 import { MuiThemeProvider } from 'material-ui/styles';
-import { Chip } from 'material-ui';
+import { AppBar } from 'material-ui';
+import { Card, CardHeader, CardText, RaisedButton } from 'material-ui';
 
 const client = new ApolloClient({
   uri: API_ROOT + '/graphql'
@@ -19,19 +18,35 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to Rehacked</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <DummyQuery />
-          <MuiThemeProvider>
-            <Chip>Text Chip</Chip>
-          </MuiThemeProvider>
-        </div>
+        <MuiThemeProvider>
+          <AppBar title='Payr' style={{ marginBottom: '50px' }} />
+          <div style={{ padding: '0 50px' }} >
+            <div style={{ width: '90%', margin: '50px auto' }} >
+              <Card containerStyle={{ padding: '30px' }}>
+                <CardHeader
+                  title="Employees"
+                  subtitle="Benefits Information"
+                  actAsExpander={false}
+                  showExpandableButton={false}
+                  style={{ marginBottom: '30px' }}
+                />
+                <div style={{  }}>
+                  <RaisedButton label="Add Employee" primary={true} style={{ }} />
+                </div>
+                <div style={{ margin: '12px 0' }}>
+                  <Card>
+                      <CardHeader 
+                        title={<div><span style={{ marginRight: '20px' }}>Adam Estela</span><span style={{ color: '#69F0AE' }}>$2500</span></div>}
+                        actAsExpander={true} 
+                        showExpandableButton={true} 
+                      />
+                      <CardText expandable={true}>Hello!</CardText>
+                  </ Card>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </ MuiThemeProvider>
       </ApolloProvider>
     );
   }
